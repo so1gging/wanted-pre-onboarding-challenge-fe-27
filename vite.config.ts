455a3793fea67d9a3706f58aae2337def92ec0ac
김솔지 <so1gging@gmail.com>
2024-10-19 15:19:06 +0900

    절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@/services', replacement: '@/services' },
+      { find: '@/stores', replacement: '@/stores' },
+      { find: '@/components', replacement: 'src/components' },
+    ],
+  },
 })
