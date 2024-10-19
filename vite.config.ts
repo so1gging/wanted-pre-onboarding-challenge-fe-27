52f02d0c320d1352b1332c8c8b58dbabeaaeb1a9
김솔지 <so1gging@gmail.com>
2024-10-19 15:55:16 +0900

    타입스크립트 환경에서 절대경로 가 적용되도록

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@/services', replacement: 'src/services' },
-      { find: '@/stores', replacement: 'src/stores' },
-      { find: '@/components', replacement: 'src/components' },
+      { find: '@/services', replacement: path.resolve(__dirname, 'src/services') },
+      { find: '@/stores', replacement: path.resolve(__dirname, 'src/stores') },
+      { find: '@/components', replacement: path.resolve(__dirname, 'src/components') },
     ],
   },
 })
