8118affa4f3ba98146be7030e476157e2437b1c2
김솔지 <so1gging@gmail.com>
2024-10-19 19:41:50 +0900

    분리된 코드 파일들 한 파일로 정리

--- src/services/apis/todos/getTodos.ts
+++
@@ -1,9 +0,0 @@
-import GetTodoOut from '@/services/models/todos/GetTodosOut'
-import { request } from '../client'
-
-const getTodos = async () => {
-  const response = await request<GetTodoOut[]>({ method: 'GET', url: 'todos' })
-  return response?.data
-}
-
-export default getTodos
